616a503fe4cd755ac0c7037acc0b54ff4044e63e
Fizza Arif <farif@hawk.iit.edu>
2018-11-30 15:15:43 -0600

    Add missing semi-colon

--- site.js
+++ site.js
@@ -17,7 +17,7 @@ navigation.insertBefore(nav_heading, nav_list);
 var menu_button = document.querySelector('#menu-button');
 menu_button.addEventListener('click', function() {
   nav_list.classList.toggle('visible');
-})
+});
 
 // change the cursor when hovering over the menu button
   document.querySelector("#menu-button").style.cursor = "pointer";
